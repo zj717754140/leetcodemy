bebb46718333a65348d0494ce48c35aaa2d81d22
zj717754140 <--global>
2020-06-15 11:26:25 +0800

    second reversed

--- sixMonth/addTwoNumbers.py
+++ sixMonth/addTwoNumbers.py
@@ -1,8 +1,16 @@
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
 
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        pass
+    def addTwoNumbers(self, nums, target):
+        for i, data in enumerate(nums):
+            # a = target - data
+            # if a in nums:
+            #    if i != nums.index(a):
+            #        return [i, nums.index(a)]
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+a = [2,7,11,15]
+target = 9
+solu = Solution()
+print(solu.addTwoNumbers(a,target))
